29838fa310e41c7f2366c70b549978b5c8fb9f37
Noluthando Mphande <67310703+nolu-thando@users.noreply.github.com>
2025-04-18 18:21:05 +0200

    Update nolu_main.py

--- nolu_main.py
+++ nolu_main.py
@@ -1,1 +1,20 @@
+from pet import Pet
 
+# Creating a Pluto
+dog = Pet("Pluto")
+
+# Interacting with Pluto
+dog.get_status()
+dog.eat()
+dog.play()
+dog.sleep()
+dog.train("sit")
+dog.train("roll over")
+dog.train("fetch")
+dog.train("shake paw")
+
+# Check Pluto's status
+dog.get_status()
+
+# Show the tricks Pluto learnt
+dog.show_tricks()
